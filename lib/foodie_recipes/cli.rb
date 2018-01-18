863680fc4dfafb9a43674fd7e76b43fd69559a1e
Maria Balsamo <mia.balsamo@gmail.com>
2018-01-18 18:31:33 +0000

    updates cli menu

--- lib/foodie_recipes/cli.rb
+++ lib/foodie_recipes/cli.rb
@@ -5,6 +5,7 @@ class FoodieRecipes::CLI
     puts "The Best Foodie Recipes:"
     list_recipes
     menu
+    goodbye
   end
 
   def list_recipes
@@ -16,17 +17,27 @@ class FoodieRecipes::CLI
   end
 
   def menu
-    puts "Enter the number or name of the recipe you would like to cook:"
-    input = gets.strip
-
-    case input
-    when "1"
-      puts "Food A Ingredients and Instructions:"
-    when "2"
-      puts "Food B Ingredients and Instructions:"
-    when "3"
-      puts "Food A Ingredients and Instructions:"
+    input = nil
+    while input != "exit"
+      puts "Enter the number or name of the recipe you would like to cook, type recipes to see the list of recipes, or type exit:"
+      input = gets.strip.downcase
+      case input
+      when "1"
+        puts "Food A Ingredients and Instructions:"
+      when "2"
+        puts "Food B Ingredients and Instructions:"
+      when "3"
+        puts "Food A Ingredients and Instructions:"
+      when "recipes"
+        list_recipes
+      else
+        puts "Not an available option. Try again."
+      end
     end
+  end
+
+  def goodbye
+    puts "Happy cooking!"
   end
 
 end
