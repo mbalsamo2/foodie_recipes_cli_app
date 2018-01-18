803279ac881c565024145f02ac826a84bc26074c
Maria Balsamo <mia.balsamo@gmail.com>
2018-01-18 18:10:49 +0000

    adds beginning structure of CLI

---
+++ lib/foodie_recipes.rb
@@ -0,0 +1,7 @@
+require_relative "./foodie_recipes/version"
+require_relative "./foodie_recipes/cli"
+# require 'open-uri'
+# require 'pry'
+# require 'nokogiri'
+
+# require_relative "foodie_recipes/recipes"
--- lib/foodie_recipes/cli.rb
+++ lib/foodie_recipes/cli.rb
@@ -1,3 +1,32 @@
 class FoodieRecipes::CLI
 
+  def call
+    puts "Welcome to Foodie Recipes, where you will find recipes to inspire your inner chef!"
+    puts "The Best Foodie Recipes:"
+    list_recipes
+    menu
+  end
+
+  def list_recipes
+    puts <<-DOC
+    1. Food A
+    2. Food B
+    3. Food C
+    DOC
+  end
+
+  def menu
+    puts "Enter the number or name of the recipe you would like to cook:"
+    input = gets.strip
+
+    case input
+    when "1"
+      puts "Food A Ingredients and Instructions:"
+    when "2"
+      puts "Food B Ingredients and Instructions:"
+    when "3"
+      puts "Food A Ingredients and Instructions:"
+    end
+  end
+
 end
