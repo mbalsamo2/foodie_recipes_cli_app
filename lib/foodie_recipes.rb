dff6b6244125355172459a06189c5311b2747dc9
Maria Balsamo <mia.balsamo@gmail.com>
2018-01-18 20:00:16 +0000

    begins to scrape

--- lib/foodie_recipes.rb
+++ lib/foodie_recipes.rb
@@ -1,6 +1,7 @@
 require_relative "./foodie_recipes/version"
 require_relative "./foodie_recipes/cli"
 require_relative "./foodie_recipes/recipes"
-# require 'open-uri'
-# require 'pry'
-# require 'nokogiri'
+
+require 'open-uri'
+require 'pry'
+require 'nokogiri'
--- lib/foodie_recipes/recipes.rb
+++ lib/foodie_recipes/recipes.rb
@@ -1,4 +1,5 @@
 class FoodieRecipes::Recipes
+  attr_accessor :name, :ingredients, :instructions, :url
 
   def self.all
     puts <<-DOC
@@ -6,6 +7,24 @@ class FoodieRecipes::Recipes
     2. Food B
     3. Food C
     DOC
+
+    doc = Nokogiri::HTML(open("https://www.browneyedbaker.com/category/recipes/main-dish-recipes/chicken-recipes/"))
+    binding.pry
+    doc.css("div.archive-post").each do |recipe|
+      name = css("h4").text
+      url = css("a").attribute("href").value
+    end
+
+    recipe_1 = self.new
+    recipe_1.name = "Crispy Baked Chicken Wings"
+    recipe_1.ingredients = "list of stuff"
+    recipe_1.instructions = "list of directions"
+    recipe_1.url = "url"
+
+  end
+
+  def self.scrape_recipes
+
   end
 
 end
