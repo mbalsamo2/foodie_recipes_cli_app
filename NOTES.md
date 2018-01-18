44c7918c606da3d8d89ceb958248ba2beabf400d
Maria Balsamo <mia.balsamo@gmail.com>
2018-01-18 19:26:24 +0000

    add some notes and refactor some things

---
+++ NOTES.md
@@ -0,0 +1,4 @@
+1. create recipe attributes (name, ingredients, instructions, url?)
+2. scrape the sights
+3. make sure they are objects!
+4. refractor menu?
--- lib/foodie_recipes/cli.rb
+++ lib/foodie_recipes/cli.rb
@@ -2,14 +2,14 @@ class FoodieRecipes::CLI
 
   def call
     puts "Welcome to Foodie Recipes, where you will find recipes to inspire your inner chef!"
-    puts "The Best Foodie Recipes:"
     list_recipes
     menu
-    goodbye
   end
 
   def list_recipes
+    puts "The Best Foodie Recipes:"
     @recipes = FoodieRecipes::Recipes.all
+
   end
 
   def menu
@@ -17,15 +17,25 @@ class FoodieRecipes::CLI
     while input != "exit"
       puts "Enter the number or name of the recipe you would like to cook, type recipes to see the list of recipes, or type exit:"
       input = gets.strip.downcase
+
+      # if input.to_i > 0
+      #   puts @recipes[input.to_i - 1]
+      # elsif input == "recipes"
+      #   list_recipes
+      # else
+      #   puts "Not an available option. Try again."
+      # end
       case input
       when "1"
-        puts "Food A Ingredients and Instructions:"
+        puts "Food A instructions and ingredients:"
       when "2"
-        puts "Food B Ingredients and Instructions:"
+        puts "Food B instructions and ingredients:"
       when "3"
-        puts "Food A Ingredients and Instructions:"
+        puts "Food C instructions and ingredients:"
       when "recipes"
         list_recipes
+      when "exit"
+        goodbye
       else
         puts "Not an available option. Try again."
       end
