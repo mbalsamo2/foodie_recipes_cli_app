7f9ce7099eebdd7aa9574db9a30c014ee39040ce
Maria Balsamo <mia.balsamo@gmail.com>
2018-01-18 20:56:13 +0000

    scrapes for instructions and ingredients

--- lib/foodie_recipes/recipes.rb
+++ lib/foodie_recipes/recipes.rb
@@ -8,22 +8,34 @@ class FoodieRecipes::Recipes
     3. Food C
     DOC
 
-    doc = Nokogiri::HTML(open("https://www.browneyedbaker.com/category/recipes/main-dish-recipes/chicken-recipes/"))
-    binding.pry
-    doc.css("div.archive-post").each do |recipe|
-      name = css("h4").text
-      url = css("a").attribute("href").value
-    end
-
     recipe_1 = self.new
     recipe_1.name = "Crispy Baked Chicken Wings"
     recipe_1.ingredients = "list of stuff"
     recipe_1.instructions = "list of directions"
     recipe_1.url = "url"
-
   end
 
   def self.scrape_recipes
+    recipes = []
+    doc = Nokogiri::HTML(open("https://www.browneyedbaker.com/category/recipes/main-dish-recipes/chicken-recipes/"))
+    doc.css("div.archive-post").each do |recipe|
+      name = css("h4").text
+      url = css("a").attribute("href").value
+    end
+  end
+
+  def self.scrape_recipe_details
+
+    doc = Nokogiri::HTML(open("https://www.browneyedbaker.com/category/recipes/main-dish-recipes/chicken-recipes/"))
+    links = doc.css("div.archive-post a").map do |i|
+      i.attribute("href").value
+    end
+
+    links.map do |recipe_url|
+      recipe_info = Nokogiri::HTML(open("#{recipe_url}"))
+      recipe_info.css("div.recipe div.ingredients ul").text.split(" ,")
+      recipe_info.css("div.instructions").text.split(" ,")
+    end
 
   end
 
