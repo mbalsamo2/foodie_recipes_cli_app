306c7faf27bfe356a5c716bbdf477079299ff9e6
Maria Balsamo <mia.balsamo@gmail.com>
2018-01-19 15:16:34 +0000

    scrapes for attributes

--- lib/foodie_recipes/recipes.rb
+++ lib/foodie_recipes/recipes.rb
@@ -8,19 +8,45 @@ class FoodieRecipes::Recipes
     3. Food C
     DOC
 
-    recipe_1 = self.new
-    recipe_1.name = "Crispy Baked Chicken Wings"
-    recipe_1.ingredients = "list of stuff"
-    recipe_1.instructions = "list of directions"
-    recipe_1.url = "url"
+    links = doc.css("div.archive-post a").map do |i|
+      i.attribute("href").value
+    end
+
+    links.map do |recipe_url|
+      recipe_info = Nokogiri::HTML(open("#{recipe_url}"))
+      binding.pry
+        # recipe_info.each do |details|
+
+          details.css("div.recipe div.ingredients ul").text
+          details.css("div.instructions").text
+        # end
+    end
+
+    # recipe_1 = self.new
+    # recipe_1.name = "Crispy Baked Chicken Wings"
+    # recipe_1.ingredients = "list of stuff"
+    # recipe_1.instructions = "list of directions"
+    # recipe_1.url = "url"
   end
 
   def self.scrape_recipes
-    recipes = []
     doc = Nokogiri::HTML(open("https://www.browneyedbaker.com/category/recipes/main-dish-recipes/chicken-recipes/"))
+
+    recipe = self.new
+
     doc.css("div.archive-post").each do |recipe|
-      name = css("h4").text
-      url = css("a").attribute("href").value
+        recipes.name = recipe.css("h4").text
+        recipes.url = recipe.css("a").attribute("href").value
+    end
+
+    links = doc.css("div.archive-post a").map do |i|
+      i.attribute("href").value
+    end
+
+    links.map do |recipe_url|
+      recipe_info = Nokogiri::HTML(open("#{recipe_url}"))
+        recipes.ingredients = details.css("div.recipe div.ingredients ul").text
+        recipes.instructions = details.css("div.instructions").text
     end
   end
 
@@ -33,10 +59,13 @@ class FoodieRecipes::Recipes
 
     links.map do |recipe_url|
       recipe_info = Nokogiri::HTML(open("#{recipe_url}"))
-      recipe_info.css("div.recipe div.ingredients ul").text.split(" ,")
-      recipe_info.css("div.instructions").text.split(" ,")
+        recipe_info.each do |details|
+          details.css("div.recipe div.ingredients ul").text
+          details.css("div.instructions").text
+        end
     end
-
+    # recipe_info.css("div.recipe div.ingredients ul").text.split(" ,")
+    # recipe_info.css("div.instructions").text.split(" ,")
   end
 
 end
