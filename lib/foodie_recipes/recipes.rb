ba217a51aeb4531db1332e5c66c8815553bc83cc
Maria Balsamo <mia.balsamo@gmail.com>
2018-01-21 21:58:47 +0000

    refactor scrape loop

--- lib/foodie_recipes/recipes.rb
+++ lib/foodie_recipes/recipes.rb
@@ -1,9 +1,6 @@
 class FoodieRecipes::Recipes
   attr_accessor :name, :ingredients, :instructions, :url
 
-  @@storage = []
-  @@recipes = []
-
   def self.scrape_recipes
     @recipes = []
 
@@ -17,31 +14,20 @@ class FoodieRecipes::Recipes
 
     recipe = self.new
 
-    doc.css("div.archive-post").each do |recipe_attr|
+    doc.css("div.archive-post").map do |recipe_attr|
       recipe.name = recipe_attr.css("h4").text
       recipe.url = recipe_attr.css("a").attribute("href").value
+      recipe_info = Nokogiri::HTML(open("#{recipe.url}"))
+      ingredients = recipe_info.css("li.ingredient").map { |ingr| ingr.text}
+      new_ingr = ingredients.join("\n")
+      recipe.ingredients = new_ingr
+      recipe.instructions = recipe_info.css("div.instructions").text
     end
-
-    recipe_info = Nokogiri::HTML(open("#{recipe.url}"))
-
-
-    ingredients = recipe_info.css("li.ingredient").map { |ingr| ingr.text}
-    new_ingr = ingredients.join("\n")
-    recipe.ingredients = new_ingr
-    recipe.instructions = recipe_info.css("div.instructions").text
-
     recipe
-
-    # @@storage << {
-    #   :name => recipe.name,
-    #   :url => recipe.url,
-    #   :ingredients => recipe.ingredients,
-    #   :instructions => recipe.instructions
-    # }
-    # @@storage
   end
 
 end
+
 
 # last working **with single recipe**
 
@@ -91,29 +77,91 @@ end
 
 # ******last with iteration of recipes****
 
-# def self.scrape_recipe_info
-#   doc = Nokogiri::HTML(open("https://www.browneyedbaker.com/category/recipes/main-dish-recipes/chicken-recipes/"))
+# class FoodieRecipes::Recipes
+#   attr_accessor :name, :ingredients, :instructions, :url
 #
-#   recipe = self.new
+#   @@storage = []
+#   @@recipes = []
 #
-#   doc.css("div.archive-post").each do |recipe_attr|
+#   def self.scrape_recipes
+#     @recipes = []
 #
-#       recipe.name = recipe_attr.css("h4").text
-#       recipe.url = recipe_attr.css("a").attribute("href").value
+#     @recipes << self.scrape_recipe_info
+#
+#     @recipes
 #   end
 #
-#   recipe_info = Nokogiri::HTML(open("#{recipe.url}"))
-#   recipe.ingredients = recipe_info.css("div.recipe div.ingredients ul").text.scan(/[^\d]*\d/)
-#   recipe.instructions = recipe_info.css("div.instructions").text
+#   def self.scrape_recipe_info
+#     doc = Nokogiri::HTML(open("https://www.browneyedbaker.com/category/recipes/main-dish-recipes/chicken-recipes/"))
 #
-#   # links = doc.css("div.archive-post a").map do |i|
-#   #   i.attribute("href").value
-#   # end
-#   #
-#   # links.each do |recipe_url|
-#   #   recipe_info = Nokogiri::HTML(open("#{recipe_url}"))
-#   #     recipe.ingredients = recipe_info.css("div.recipe div.ingredients ul").text.scan(/[^\d]*\d/)
-#   #     recipe.instructions = recipe_info.css("div.instructions").text
-#   # end
-#   recipe
+#     recipe = self.new
+#
+#     doc.css("div.archive-post").each do |recipe_attr|
+#       recipe.name = recipe_attr.css("h4").text
+#       recipe.url = recipe_attr.css("a").attribute("href").value
+#     end
+#
+#     recipe_info = Nokogiri::HTML(open("#{recipe.url}"))
+#
+#     ingredients = recipe_info.css("li.ingredient").map { |ingr| ingr.text}
+#     new_ingr = ingredients.join("\n")
+#     recipe.ingredients = new_ingr
+#     recipe.instructions = recipe_info.css("div.instructions").text
+#
+#     recipe
+#    end
+# end
+
+# ***Possible Fix?****
+# class FoodieRecipes::Recipes
+#   attr_accessor :name, :ingredients, :instructions, :url
+#
+#   @@storage = []
+#   @@recipes = []
+#
+#   def self.scrape_recipes
+#     @recipes = []
+#
+#     @recipes << self.scrape_recipe_info
+#
+#     @recipes
+#   end
+#
+#   def self.scrape_recipe_info
+#     doc = Nokogiri::HTML(open("https://www.browneyedbaker.com/category/recipes/main-dish-recipes/chicken-recipes/"))
+#
+#     recipe = []
+#
+#     doc.css("div.archive-post").map do |recipe_attr|
+#       name = recipe_attr.css("h4").text
+#       url = recipe_attr.css("a").attribute("href").value
+#       recipe_info = Nokogiri::HTML(open("#{url}"))
+#       ingredients = recipe_info.css("li.ingredient").map { |ingr| ingr.text}
+#       new_ingr = ingredients.join("\n")
+#       ingredients = new_ingr
+#       instructions = recipe_info.css("div.instructions").text
+#       @@recipes << {
+#         :name => name,
+#         :url => url,
+#         :ingredients => ingredients,
+#         :instructions => instructions
+#       }
+#     end
+#   binding.pry
+#     @@recipes
+#   end
+#
+#   def self.create_from_scrape
+#     @@recipes.each do |key, value|
+#
+#       self.send "#{key}=", value
+#       self.new
+#       @@storage << self
+#     end
+#   end
+#
+#   def self.storage
+#     @@storage
+#   end
+#
 # end
