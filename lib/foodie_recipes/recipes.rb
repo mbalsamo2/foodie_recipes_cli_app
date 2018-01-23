f5475549d007755f33abfafc0e44b49d2ddca83f
Maria Balsamo <mia.balsamo@gmail.com>
2018-01-23 16:34:08 +0000

    rebuilds with scraper class

--- lib/foodie_recipes/recipes.rb
+++ lib/foodie_recipes/recipes.rb
@@ -1,30 +1,35 @@
 class FoodieRecipes::Recipes
   attr_accessor :name, :ingredients, :instructions, :url
 
-  def self.scrape_recipes
-    @recipes = []
 
-    @recipes << self.scrape_recipe_info
-
-    @recipes
+  def initialize(name = nil, url = nil, ingredients = nil, instructions = nil)
+    @name = name
+    @url = url
+    @ingredients = ingredients
+    @instructions = instructions
   end
-
-  def self.scrape_recipe_info
-    doc = Nokogiri::HTML(open("https://www.browneyedbaker.com/category/recipes/main-dish-recipes/chicken-recipes/"))
-
-    recipe = self.new
-
-    doc.css("div.archive-post").map do |recipe_attr|
-      recipe.name = recipe_attr.css("h4").text
-      recipe.url = recipe_attr.css("a").attribute("href").value
-      recipe_info = Nokogiri::HTML(open("#{recipe.url}"))
-      ingredients = recipe_info.css("li.ingredient").map { |ingr| ingr.text}
-      new_ingr = ingredients.join("\n")
-      recipe.ingredients = new_ingr
-      recipe.instructions = recipe_info.css("div.instructions").text
-    end
-    recipe
-  end
+  # def self.scrape_recipes
+  #   @recipes = []
+  #
+  #   @recipes << self.scrape_recipe_info
+  #
+  #   @recipes
+  # end
+  #
+  # def self.scrape_recipe_info
+  #   recipe = self.new
+  #
+  #   doc.map do |recipe_attr|
+  #     recipe.name = recipe_attr.css("h4").text
+  #     recipe.url = recipe_attr.css("a").attribute("href").value
+  #     recipe_info = Nokogiri::HTML(open("#{recipe.url}"))
+  #     ingredients = recipe_info.css("li.ingredient").map { |ingr| ingr.text}
+  #     new_ingr = ingredients.join("\n")
+  #     recipe.ingredients = new_ingr
+  #     recipe.instructions = recipe_info.css("div.instructions").text
+  #   end
+  #   recipe
+  # end
 
 end
 
--- lib/foodie_recipes/scraper.rb
+++ lib/foodie_recipes/scraper.rb
@@ -1,2 +1,17 @@
-class FoodieRecipes::Recipes
+class FoodieRecipes::Scraper
+
+  def get_website
+    Nokogiri::HTML(open("https://www.browneyedbaker.com/category/recipes/main-dish-recipes/chicken-recipes/"))
+  end
+
+  def scrape_the_recipes
+    self.get_website.css("div.archive-post")
+  end
+
+  def create_recipes
+    scrape_the_recipes.each do |recipe|
+      #initialize recipe within recipe.rb
+    end
+  end
+
 end
